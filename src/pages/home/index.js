67c38beaba57c960a442a1d90c37810b04c2cdec
camillebogda <camille.bogda@gmail.com>
2026-06-22 11:59:58 +0200

    Refactor home page content and button links
    Updated the home page content and layout, replacing Typewriter component with static text and modifying button links.

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,16 +1,17 @@
 import React from "react";
 import "./style.css";
 import { Helmet, HelmetProvider } from "react-helmet-async";
-import Typewriter from "typewriter-effect";
 import { introdata, meta } from "../../config/siteContent";
 import { Link } from "react-router-dom";
 
 export const Home = () => {
   const pageTitle = `Accueil | ${meta.title}`;
   const pageDescription = meta.description;
+
   return (
     <HelmetProvider>
       <section id="home" className="home">
+
         <Helmet>
           <meta charSet="utf-8" />
           <title>{pageTitle}</title>
@@ -20,51 +21,62 @@ export const Home = () => {
           <meta property="og:description" content={pageDescription} />
           <meta property="og:type" content="website" />
         </Helmet>
-        <div className="intro_sec d-block d-lg-flex align-items-center ">
+
+        <div className="intro_sec d-block d-lg-flex align-items-center">
+
+          {/* IMAGE (modifiée via introdata.your_img_url) */}
           <div
-            className="h_bg-image order-1 order-lg-2 h-100 "
+            className="h_bg-image order-1 order-lg-2 h-100"
             style={{ backgroundImage: `url(${introdata.your_img_url})` }}
           ></div>
+
+          {/* TEXTE */}
           <div className="text order-2 order-lg-1 h-100 d-lg-flex justify-content-center">
-            <div className="align-self-center ">
+            <div className="align-self-center">
+
               <div className="intro mx-auto">
-                <h2 className="mb-1x">{introdata.title}</h2>
+
+                <h2 className="mb-1x">
+                  Photographie & direction de l’image
+                </h2>
+
                 <h1 className="fluidz-48 mb-1x">
-                  <Typewriter
-                    options={{
-                      strings: [
-                        introdata.animated.first,
-                        introdata.animated.second,
-                        introdata.animated.third,
-                      ],
-                      autoStart: true,
-                      loop: true,
-                      deleteSpeed: 10,
-                    }}
-                  />
+                  Photographie d’entreprise<br />
+                  Direction de la photographie<br />
+                  Lumière & production plateau
                 </h1>
-                <p className="mb-1x">{introdata.description}</p>
+
+                <p className="mb-1x">
+                  J’accompagne entreprises et productions dans la création d’images fortes :
+                  portraits corporate, communication visuelle et direction artistique sur plateau.
+                </p>
+
                 <div className="intro_btn-action pb-5">
-                  <Link to="/portfolio" className="text_2">
-                    <div id="button_p" className="ac_btn btn ">
-                      Mon portfolio
+
+                  <Link to="/photo" className="text_2">
+                    <div id="button_p" className="ac_btn btn">
+                      Voir mes services
                       <div className="ring one"></div>
                       <div className="ring two"></div>
                       <div className="ring three"></div>
                     </div>
                   </Link>
+
                   <Link to="/contact">
                     <div id="button_h" className="ac_btn btn">
-                      Contactez-moi
+                      Demander un devis
                       <div className="ring one"></div>
                       <div className="ring two"></div>
                       <div className="ring three"></div>
                     </div>
                   </Link>
+
                 </div>
+
               </div>
             </div>
           </div>
+
         </div>
       </section>
     </HelmetProvider>
