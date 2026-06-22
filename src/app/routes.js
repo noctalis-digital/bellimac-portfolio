e7489e97f410e24ac6e2103727c78d55136ebf2d
camillebogda <camille.bogda@gmail.com>
2026-06-22 11:32:14 +0200

    Remove About page import from routes
    Removed import statement for the About page.

--- src/app/routes.js
+++ src/app/routes.js
@@ -4,7 +4,6 @@ import withRouter from "../hooks/withRouter"
 import { Home } from "../pages/home";
 import { Portfolio } from "../pages/portfolio";
 import { ContactUs } from "../pages/contact";
-import { About } from "../pages/about";
 import { Socialicons } from "../components/socialicons";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 
